22aaea12712887a15b23663cc05fbf5961594b78
UPGLE <admin@upgle.com>
2015-07-16 14:40:27 +0900

    gruntfile reformat

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,53 +1,52 @@
 
 module.exports = function(grunt) {
 
-  var gh_pages = { config: grunt.file.readYAML('gh-pages/_config.yml') };
+    var gh_pages = { config: grunt.file.readYAML('gh-pages/_config.yml') };
 
-  grunt.initConfig({
+    grunt.initConfig({
+        'copy': {
+            'main': {
+                expand: true,
+                cwd: 'kr/',
+                src: ['*.md', '!documentation.md'],
+                dest: 'gh-pages/docs/'
+            },
+        },
+        'gh-pages': {
+            options: {
+                base: 'gh-pages',
+                repo: 'https://github.com/xpressengine/laravel-korean-docs.git'
+            },
+            src: ['**/*']
+        },
+        'build': {
+            kr_docs: {
+                src: 'gh-pages/docs/*'
+            },
+            options: {
+                permalink_prefix: '/docs/5.0/'
+            }
+        }
+    });
 
-    'copy': {
-      'main': {
-        expand: true, 
-        cwd: 'kr/',
-        src: ['*.md', '!documentation.md'],
-        dest: 'gh-pages/docs/',
-      },
-    },
-    'gh-pages': {
-      options: { 
-      	base: 'gh-pages',
-		repo: 'https://github.com/xpressengine/laravel-korean-docs.git'
-	  },
-      src: ['**/*']
-    },
-    'build': {
-      kr_docs: {
-        src: 'gh-pages/docs/*'
-      },
-      options: { 
-        permalink_prefix : '/docs/5.0/'
-      }
-    }
-  });
-
-  grunt.registerMultiTask('build', 'build gh-pages for jeykll', function () {
+    grunt.registerMultiTask('build', 'build gh-pages for jeykll', function () {
 
         var options = this.options({
-          permalink_prefix: /docs/,
+            permalink_prefix: /docs/,
         });
 
         this.files.forEach(function (files) {
 
-            files.src.forEach(function(file) {
+            files.src.forEach(function (file) {
 
-              var  content = grunt.file.read(file),
-                firstLine = content.split('\n')[0].replace("# ", ""),
-                filename = file.replace(/^.*[\\\/]/, '').replace(/.[^.]+$/,''),
-                prepend = '',
-                append = '';
+                var content = grunt.file.read(file),
+                    firstLine = content.split('\n')[0].replace("# ", ""),
+                    filename = file.replace(/^.*[\\\/]/, '').replace(/.[^.]+$/, ''),
+                    prepend = '',
+                    append = '';
 
-                if(content.charAt(0) !== '#') return;
-            
+                if (content.charAt(0) !== '#') return;
+
                 prepend = '---\n';
                 prepend += 'layout: default\n';
                 prepend += 'title: "' + firstLine + '"\n';
@@ -58,10 +57,10 @@ module.exports = function(grunt) {
                 content = content.replace(/([\{|\}]{2,})/g, '{% raw %}$1{% endraw %}');
                 content = content.replace(/\((\/docs\/)/g, '(' + gh_pages.config.baseurl + '$1');
 
-                content = content.replace(/<!--chak-comment-(.+)-->/g, '<div class="chak-comment-wrap"><div class="chak-comment-widget" data-apikey="coe00da03b685a0dd18fb6a08af0923de0-laravel-korean-docs-$1" ><i class="xi-message"></i> <strong>클릭</strong>하여 의견을 공유할 수 있습니다. ( 총 <span class="count"><i class="xi-spinner-5 xi-spin"></i></span>개의 의견이 있습니다. )</div></div>');
-                
+                content = content.replace(/<!--chak-comment-(.+)-->/g, '<div class="chak-comment-wrap"><div class="chak-comment-widget" data-chak-group="laravel" data-chak-apikey="582898af492efbcdd53990e1c6ccb89d-laravel-korean-docs-$1" ><i class="xi-message"></i> <strong>클릭</strong>하여 의견을 공유할 수 있습니다. ( 총 <span class="count"><i class="xi-spinner-5 xi-spin"></i></span>개의 의견이 있습니다. )</div></div>');
+
                 // save file
-                grunt.file.write( file, [prepend, content, append].join('') );
+                grunt.file.write(file, [prepend, content, append].join(''));
             });
 
         });
@@ -70,6 +69,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-gh-pages');
     grunt.loadNpmTasks('grunt-contrib-copy');
 
-    grunt.registerTask('default', [ 'copy', 'build']);
+    grunt.registerTask('default', ['copy', 'build']);
     grunt.registerTask('deploy', ['gh-pages']);
 };
